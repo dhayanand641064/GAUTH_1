12152504be5e84c7b9831c197938f47753756108
dhayanand ss <dhayanand641064@gmail.com>
2023-06-13 01:13:16 +0530

    UPDATED main.go

--- main.go
+++ main.go
@@ -19,21 +19,16 @@ func init() {
 }
 
 func main() {
-
 	http.HandleFunc("/", rootHandler)
-
 	http.HandleFunc("/login/github/", githubLoginHandler)
-
 	http.HandleFunc("/login/github/callback", githubCallbackHandler)
-
 	http.HandleFunc("/loggedin", func(w http.ResponseWriter, r *http.Request) {
-		loggedinHandler(w, r, "")
+		githubData := r.URL.Query().Get("githubData")
+		loggedinHandler(w, r, githubData)
 	})
 
 	fmt.Println("[ UP ON PORT 3000 ]")
-	log.Panic(
-		http.ListenAndServe(":3000", nil),
-	)
+	log.Panic(http.ListenAndServe(":3000", nil))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,16 +37,23 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func loggedinHandler(w http.ResponseWriter, r *http.Request, githubData string) {
 	if githubData == "" {
-		fmt.Fprintf(w, "UNAUTHORIZED!")
+		// Unauthorized response
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintf(w, `{"error": "Unauthorized"}`)
 		return
 	}
 
-	w.Header().Set("Content-type", "application/json")
+	// Process authorized response
+	w.Header().Set("Content-Type", "application/json")
 
 	var prettyJSON bytes.Buffer
 	parserr := json.Indent(&prettyJSON, []byte(githubData), "", "\t")
 	if parserr != nil {
-		log.Panic("JSON parse error")
+		// JSON parse error
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, `{"error": "JSON parse error"}`)
+		return
 	}
 
 	fmt.Fprintf(w, string(prettyJSON.Bytes()))
@@ -59,20 +61,27 @@ func loggedinHandler(w http.ResponseWriter, r *http.Request, githubData string) 
 
 func githubLoginHandler(w http.ResponseWriter, r *http.Request) {
 	githubClientID := getGithubClientID()
-
-	redirectURL := fmt.Sprintf("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s", githubClientID, "http://localhost:3000/login/github/callback")
-
+	redirectURL := fmt.Sprintf("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=user,read:org", githubClientID, "http://localhost:3000/login/github/callback")
 	http.Redirect(w, r, redirectURL, 301)
 }
 
 func githubCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
-
 	githubAccessToken := getGithubAccessToken(code)
-
 	githubData := getGithubData(githubAccessToken)
+	githubOrgs := getGithubOrganizations(githubAccessToken)
 
-	loggedinHandler(w, r, githubData)
+	response := struct {
+		GithubData string   `json:"githubData"`
+		GithubOrgs []string `json:"githubOrgs"`
+	}{
+		GithubData: githubData,
+		GithubOrgs: githubOrgs,
+	}
+
+	responseJSON, _ := json.Marshal(response)
+
+	http.Redirect(w, r, "/loggedin?githubData="+string(responseJSON), http.StatusSeeOther)
 }
 
 func getGithubData(accessToken string) string {
@@ -95,26 +104,31 @@ func getGithubData(accessToken string) string {
 }
 
 func getGithubAccessToken(code string) string {
-
 	clientID := getGithubClientID()
 	clientSecret := getGithubClientSecret()
 
-	requestBodyMap := map[string]string{"client_id": clientID, "client_secret": clientSecret, "code": code}
+	requestBodyMap := map[string]string{
+		"client_id":     clientID,
+		"client_secret": clientSecret,
+		"code":          code,
+	}
+
 	requestJSON, _ := json.Marshal(requestBodyMap)
 
-	req, reqerr := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(requestJSON))
-	if reqerr != nil {
-		log.Panic("Request creation failed")
+	req, reqErr := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(requestJSON))
+	if reqErr != nil {
+		log.Panic("Request creation failed:", reqErr)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
 
-	resp, resperr := http.DefaultClient.Do(req)
-	if resperr != nil {
-		log.Panic("Request failed")
+	resp, respErr := http.DefaultClient.Do(req)
+	if respErr != nil {
+		log.Panic("Request failed:", respErr)
 	}
+	defer resp.Body.Close()
 
-	respbody, _ := ioutil.ReadAll(resp.Body)
+	respBody, _ := ioutil.ReadAll(resp.Body)
 
 	type githubAccessTokenResponse struct {
 		AccessToken string `json:"access_token"`
@@ -122,28 +136,56 @@ func getGithubAccessToken(code string) string {
 		Scope       string `json:"scope"`
 	}
 
-	var ghresp githubAccessTokenResponse
-	json.Unmarshal(respbody, &ghresp)
+	var ghResp githubAccessTokenResponse
+	json.Unmarshal(respBody, &ghResp)
 
-	return ghresp.AccessToken
+	return ghResp.AccessToken
 }
 
 func getGithubClientID() string {
-
 	githubClientID, exists := os.LookupEnv("CLIENT_ID")
 	if !exists {
 		log.Fatal("Github Client ID not defined in .env file")
 	}
-
 	return githubClientID
 }
 
 func getGithubClientSecret() string {
-
 	githubClientSecret, exists := os.LookupEnv("CLIENT_SECRET")
 	if !exists {
-		log.Fatal("Github Client ID not defined in .env file")
+		log.Fatal("Github Client Secret not defined in .env file")
+	}
+	return githubClientSecret
+}
+
+func getGithubOrganizations(accessToken string) []string {
+	req, reqerr := http.NewRequest("GET", "https://api.github.com/user/orgs", nil)
+	if reqerr != nil {
+		log.Panic("API Request creation failed")
 	}
 
-	return githubClientSecret
+	authorizationHeaderValue := fmt.Sprintf("token %s", accessToken)
+	req.Header.Set("Authorization", authorizationHeaderValue)
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	defer resp.Body.Close()
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	type githubOrg struct {
+		Login string `json:"login"`
+	}
+
+	var orgs []githubOrg
+	json.Unmarshal(respbody, &orgs)
+
+	orgNames := make([]string, len(orgs))
+	for i, org := range orgs {
+		orgNames[i] = org.Login
+	}
+
+	return orgNames
 }
